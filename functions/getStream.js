3bd2877149b9354beb1c615e4a0e262ff8fd7b31
Mark Wasfy <76422492+MarkWasfy00@users.noreply.github.com>
2022-10-26 04:33:23 +0200

    added functions

---
+++ functions/getStream.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+const cheerio = require("cheerio")
+const URL = 'https://www.gofundme.com/f/creators-care-project';
+
+exports.handler = async (event, context) => {
+    const site = await axios.get(URL);
+    const $ = cheerio.load(site.data);
+    const info = $(".m-progress-meter-heading").text()
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ 
+        current: info.substring(0, info.indexOf('raised')).trim(),
+        goal: info.substring(info.indexOf('of') + 2, info.indexOf('goal')).trim()
+    }),
+    };
+};
